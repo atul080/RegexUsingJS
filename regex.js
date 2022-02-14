f94ecf08dd5404c5d10b2bc74176cee8524137f6
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-14 07:10:39 +0530

    Added: Pin code 6 digit validation functionality added.

---
+++ regex.js
@@ -0,0 +1,30 @@
+/**
+ * @Author: Atul Srivastav
+ * @Date 11-Feb-2022
+ * User Validation
+ */
+class ValidationCheck{
+      /**
+       * valueValidation method to validate pincode.
+       * if it is 6 digit then valid else throw error.
+       * @param pin 
+       */
+      valueValidation(pin) 
+        {
+              let valRegex=RegExp('^[1-9]{1}[0-9]{5}$');
+              if((valRegex.test(pin)))
+                  console.log("Valid pin code: "+pin)
+              else
+                  throw "Invalid Pincode: "+pin   
+        }
+}
+
+let obj=new ValidationCheck();
+  try{
+  obj.valueValidation(400088)
+  obj.valueValidation(4123)
+  }
+  catch(e)
+  {
+    console.error(e);
+  }
