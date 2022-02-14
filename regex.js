4a7d42de489092308fd0735f04e1ecab53810fbf
atul080 <atulsrivastav16k26@gmaial.com>
2022-02-14 07:23:57 +0530

    Added: Allowing single whitespace in pincode functionality added.

--- regex.js
+++ regex.js
@@ -1,30 +1,30 @@
-/**
- * @Author: Atul Srivastav
- * @Date 11-Feb-2022
- * User Validation
- */
-class ValidationCheck{
-      /**
-       * valueValidation method to validate pincode.
-       * if it is 6 digit then valid else throw error.
-       * @param pin 
-       */
-      valueValidation(pin) 
-        {
-              let valRegex=RegExp('^[1-9]{1}[0-9]{5}$');
-              if((valRegex.test(pin)))
-                  console.log("Valid pin code: "+pin)
-              else
-                  throw "Invalid Pincode: "+pin   
-        }
-}
+  /**
+   * @Author: Atul Srivastav
+   * @Date 11-Feb-2022
+   * User Validation
+   */
+  class ValidationCheck{
+        /**
+         * valueValidation method to validate pincode.
+         * if it is 6 digit then valid else throw error.
+         * @param pin 
+         */
+        valueValidation(pin) 
+          {
+                let valRegex=RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$');
+                if((valRegex.test(pin)))
+                    console.log("Valid pin code: "+pin)
+                else
+                    throw "Invalid Pincode: "+pin   
+          }
+  }
 
-let obj=new ValidationCheck();
-  try{
-  obj.valueValidation(400088)
-  obj.valueValidation('400088B')
-  }
-  catch(e)
-  {
-    console.error(e);
-  }
+  let obj=new ValidationCheck();
+    try{
+    obj.valueValidation('400088')
+    obj.valueValidation('400 088')
+    }
+    catch(e)
+    {
+      console.error(e);
+    }
